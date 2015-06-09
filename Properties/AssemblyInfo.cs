f91229953dc597793aa08f649c6807f609d773ac
Neil Derraugh <neil.derraugh@xpologistics.com>
2015-06-09 16:26:11 -0400

    sync and async implementations.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SixpackAB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sixpack AB Framework Client")]
+[assembly: AssemblyDescription("C# client library for SeatGeek's Sixpack AB testing framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SixpackAB")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.*")]
-[assembly: AssemblyFileVersion("0.16.0.0")]
+[assembly: AssemblyVersion("0.23.*")]
+[assembly: AssemblyFileVersion("0.23.0.0")]
--- Session.cs
+++ Session.cs
@@ -10,7 +10,7 @@ using System.Web.Script.Serialization;
 namespace SixpackAB {
     public class Session {
         private const string validName = @"^[a-z0-9][a-z0-9\-_ ]*$";
-        private readonly Guid clientId;
+        private readonly string clientId;
         private readonly string baseUrl = "http://localhost:5000";
         private readonly string ipAddress;
         private readonly string userAgent;
@@ -24,9 +24,8 @@ namespace SixpackAB {
         /// <param name="timeout">Timeout for the internal HTTP client used by the session.</param>
         /// <param name="ipAddress">The IP address of the subject of your experiment.</param>
         /// <param name="userAgent">The Useragent of the subject of your experiment.</param>
-        public Session(Guid? clientId = null, string baseUrl = null, int? timeout = null, string ipAddress = null,
-            string userAgent = null) {
-            this.clientId = clientId ?? Guid.NewGuid();
+        public Session(string clientId = null, string baseUrl = null, int? timeout = null, string ipAddress = null, string userAgent = null) {
+            this.clientId = clientId ?? Guid.NewGuid().ToString();
             this.baseUrl = baseUrl ?? this.baseUrl;
             this.ipAddress = ipAddress;
             this.userAgent = userAgent;
@@ -39,9 +38,55 @@ namespace SixpackAB {
         /// <param name="experimentName">The name of the experiment to participate in.</param>
         /// <param name="alternatives">The alternatives for the experiment.</param>
         /// <param name="force">Force an alternative, for testing purposes.</param>
+        public dynamic Participate(string experimentName, string[] alternatives, string force)
+        {
+            if (!Regex.IsMatch(experimentName, validName))
+            {
+                throw new Exception("Bad experimentName");
+            }
+            if (alternatives.Length < 2)
+            {
+                throw new Exception("Must specify at least 2 alternatives");
+            }
+            foreach (var alt in alternatives)
+            {
+                if (!Regex.IsMatch(experimentName, validName))
+                {
+                    throw new Exception("Bad alternative name: " + alt);
+                }
+            }
+            if (force != null)
+            {
+                return
+                    new
+                    {
+                        Status = "ok",
+                        Alternative = new
+                        {
+                            Name = force
+                        },
+                        Experiment = new
+                        {
+                            Version = 0,
+                            Name = experimentName
+                        },
+                        ClientId = clientId
+                    };
+            }
+            var parameters = GetParameters(experimentName);
+            parameters.Add("alternatives", alternatives);
+            return Request(baseUrl + "/participate", parameters);
+        }
+
+        /// <summary>
+        /// The client method to participate in an experiment.
+        /// </summary>
+        /// <param name="experimentName">The name of the experiment to participate in.</param>
+        /// <param name="alternatives">The alternatives for the experiment.</param>
+        /// <param name="force">Force an alternative, for testing purposes.</param>
         /// <param name="callback">The callback that will handle the result of the method call or the exception that got thrown.</param>
-        public void Participate(string experimentName, string[] alternatives, string force,
-            Action<Exception, object> callback) {
+        public void ParticipateAsync(string experimentName, string[] alternatives, string force,
+            Action<Exception, dynamic> callback) {
                 if (!Regex.IsMatch(experimentName, validName))
                 {
                 throw new Exception("Bad experimentName");
@@ -72,7 +117,15 @@ namespace SixpackAB {
             else {
                 var parameters = GetParameters(experimentName);
                 parameters.Add("alternatives", alternatives);
-                Request(baseUrl + "/participate", parameters, timeout, callback);
+                try
+                {
+                    callback(null, Request(baseUrl + "/participate", parameters));
+                }
+                catch(Exception e)
+                {
+                    callback(e, null);
+                }
+                
             }
         }
 
@@ -82,7 +135,32 @@ namespace SixpackAB {
         /// <param name="experimentName">The name of the experiment related to the conversion.</param>
         /// <param name="kpi">Any arbitrary KPI you want to associate with the conversion.</param>
         /// <param name="callback">The callback that will handle the result of the method call or the exception that got thrown.</param>
-        public void Convert(string experimentName, string kpi, Action<Exception, object> callback) {
+        public dynamic Convert(string experimentName, string kpi)
+        {
+            if (!Regex.IsMatch(experimentName, validName))
+            {
+                throw new Exception("Bad experimentName");
+            }
+
+            var parameters = GetParameters(experimentName);
+            if (kpi != null)
+            {
+                if (!Regex.IsMatch(kpi, validName))
+                {
+                    throw new Exception("Bad kpi");
+                }
+                parameters.Add("kpi", kpi);
+            }
+            return Request(baseUrl + "/convert", parameters);
+        }
+
+        /// <summary>
+        /// The client method to register an conversion in an experiment.
+        /// </summary>
+        /// <param name="experimentName">The name of the experiment related to the conversion.</param>
+        /// <param name="kpi">Any arbitrary KPI you want to associate with the conversion.</param>
+        /// <param name="callback">The callback that will handle the result of the method call or the exception that got thrown.</param>
+        public void ConvertAsync(string experimentName, string kpi, Action<Exception, dynamic> callback) {
             if (!Regex.IsMatch(experimentName, validName))
             {
                 callback(new Exception("Bad experimentName"), null);
@@ -95,7 +173,14 @@ namespace SixpackAB {
                 }
                 parameters.Add("kpi", kpi);
             }
-            Request(baseUrl + "/convert", parameters, timeout, callback);
+            try
+            {
+                callback(null, Request(baseUrl + "/convert", parameters));
+            }
+            catch (Exception e)
+            {
+                callback(e, null);
+            }
         }
 
         private Uri RequestUri(string endpoint, Dictionary<string, object> parameters) {
@@ -115,28 +200,25 @@ namespace SixpackAB {
             return new Uri(endpoint);
         }
 
-        private async void Request(string baseUrl, Dictionary<string, object> parameters, int timeout,
-            Action<Exception, object> callback) {
+        private dynamic Request(string baseUrl, Dictionary<string, object> parameters) {
+            dynamic result = null;
             var client = new HttpClient {Timeout = new TimeSpan(0, 0, 0, 0, timeout)};
             var uri = RequestUri(baseUrl, parameters);
             try {
-                var response = await client.GetAsync(uri);
+                var response = client.GetAsync(uri).Result;
                 response.EnsureSuccessStatusCode();
-                var responseJson = await response.Content.ReadAsStringAsync();
-                dynamic result = new JavaScriptSerializer().Deserialize<object>(responseJson);
-                callback(null, result);
+                var responseJson = response.Content.ReadAsStringAsync().Result;
+                result = new JavaScriptSerializer().Deserialize<object>(responseJson);
             }
             catch (HttpException e) {
                 if (e.GetHttpCode() == (int) HttpStatusCode.InternalServerError) {
-                    callback(null, new {Status = "failed", Response = e.GetHtmlErrorMessage()});
+                    return new {Status = "failed", Response = e.GetHtmlErrorMessage()};
                 }
             }
             catch (TimeoutException) {
-                callback(new Exception("request timed out"), null);
+                throw new Exception("request timed out");
             }
-            catch (Exception e) {
-                callback(e, null);
-            }
+            return result;
         }
 
         private Dictionary<string, object> GetParameters(string experimentName) {
